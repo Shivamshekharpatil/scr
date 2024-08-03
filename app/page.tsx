ec7e029d58cdc72592a279acd2a20067b18d847b
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2024-08-04 00:34:37 +0100

    started designing homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,42 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+    <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <div className="flex max-xl:flex-col gap-16">
+        <div className="flex flex-col justify-center">
+          <p className="small-text">
+          Your smart shopping starts here:
+          <Image 
+           src="/assets/icons/arrow-right.svg"
+           alt="arrow-right"
+           width={16}
+           height={16}
+          />
+          </p>
+          <h1 className="head-text">
+            Unleash the Power of
+            <span className="text-primary">ScraPely</span>
+          </h1>
+          <p className="mt-6">
+          Powerful, self-serve analytics designed to boost product growth. 
+          Effortlessly convert, engage, and retain more.
+          </p>
+          Searchbar
+        </div>
+
+        HeroCarousel
+      </div>
+    </section>
+    
+    <section className="trending-section">
+      <h2 className="section-text">Trending</h2>
+
+      <div className="flex flex-wrap gap-x-8 gap-y-16"></div>
+    </section>
+    </>
   )
 }
 
