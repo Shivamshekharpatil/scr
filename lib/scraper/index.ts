649f77cddbc6ac39bb14830f24398d02a8c29885
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2024-08-05 13:21:05 +0100

    setting up scrapping.

---
+++ lib/scraper/index.ts
@@ -0,0 +1,21 @@
+export async function scrapeAmazonProduct(url: string) {
+    if(!url) return;
+
+    /* curl --proxy brd.superproxy.io:22225 --proxy-user brd-customer-hl_3dfce9a0-zone-scrapely:wsihx2ap7upq -k 
+    "https://geo.brdtest.com/mygeo.json" */
+
+    //BrightData proxy configuration
+    const username = String(process.env.BRIGHT_DATA_USERNAME);
+    const password = String(process.env.BRIGHT_DATA_PASSWORD);
+    const port = 22225;
+    const session_id = (1000000 * Math.random()) | 0;
+    const options = {
+        auth: {
+            username: `${username}-session-${session_id}`,
+            password,
+        },
+        host: 'brd.superproxy.io',
+        port,
+        rejectUnauthorized: false,
+    }
+}
