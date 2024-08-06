dbd1b328a578c9cc6e72536d0e430dc06a17db7e
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2024-08-06 15:17:05 +0100

    axios - cheerio package added which will help us to parse HTML string.

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import * as cheerio from 'cheerio';
+
 export async function scrapeAmazonProduct(url: string) {
     if(!url) return;
 
@@ -17,5 +20,14 @@ export async function scrapeAmazonProduct(url: string) {
         host: 'brd.superproxy.io',
         port,
         rejectUnauthorized: false,
+    }
+
+    try {
+        //Fetch the product page.
+        const response = await axios.get(url, options);
+
+        console.log(response.data);
+    } catch (error: any) {
+        throw new Error(`Failed to scrape product: ${error.message}`)
     }
 }
